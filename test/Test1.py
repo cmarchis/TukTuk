78d9dd892dccd8413106c9f8ec4ae520e5b6ac5b
Calin Marchis <calin.marchis@hpe.com>
2016-09-27 15:21:29 +0300

    update Test1

--- test/Test1.py
+++ test/Test1.py
@@ -3,7 +3,7 @@ from tools.DriverUtils import DriverUtils
 from tools.WebdriverBase import WebdriverBase
 from pages.MenuNavigationPage import MenuNavigationPage
 from pages.DeploymentsMenuHeaderPage import DeploymentsMenuHeaderPage
-
+from pages.DeploymentsPage import DeploymentsPage
 
 
 class Test1(unittest.TestCase):
@@ -11,14 +11,14 @@ class Test1(unittest.TestCase):
         self.browser = DriverUtils().start_driver()
 
     def test_Test1(self):
-        menu_navigation_page=MenuNavigationPage(self.browser)
-        menu_navigation_page.navigate_to("http://172.22.140.63:8014/provision")
-        menu_navigation_page.navigate_to("http://172.22.140.63:8014/provision")
+        menu_navigation_page = MenuNavigationPage(self.browser)
+        menu_navigation_page.navigate_to("http://localhost:8014/provision")
+        menu_navigation_page.navigate_to("http://localhost:8014/provision")
         menu_navigation_page.click_on_menu_item("Deployments")
-        deployments_menu_header_page=DeploymentsMenuHeaderPage(self.browser)
+        deployments_menu_header_page = DeploymentsMenuHeaderPage(self.browser)
         deployments_menu_header_page.click_on_header_menu_item("Scan Compliance")
-
-
+        deployment_page = DeploymentsPage(self.browser)
+        print "list :", deployment_page.create_list_of_dictionary_for_policies()
 
     def tearDown(self):
         menuNavigationPage = MenuNavigationPage(self.browser)
