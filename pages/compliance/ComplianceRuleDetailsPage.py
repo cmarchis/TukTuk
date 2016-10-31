cab77848f0022ca18f627c0ae9c11b14fa3cbe39
Calin Marchis <calin.marchis@hpe.com>
2016-10-31 18:15:33 +0200

    update ComplianceRuleDetailsPage

--- pages/compliance/ComplianceRuleDetailsPage.py
+++ pages/compliance/ComplianceRuleDetailsPage.py
@@ -1,7 +1,6 @@
 from tools.WebdriverBase import WebdriverBase
 
-COMPLIANCE_RULE_DETAILS_CONTAINER='div.grommetux-box--basis-large'
-
+COMPLIANCE_RULE_DETAILS_CONTAINER = 'div.grommetux-box--basis-large'
 
 
 class ComplianceRuleDetailsPage(WebdriverBase):
@@ -10,8 +9,14 @@ class ComplianceRuleDetailsPage(WebdriverBase):
 
     def create_compliance_rule_details_list_of_dictionary(self):
         compliance_container = self.locate_element_by_css_selector(COMPLIANCE_RULE_DETAILS_CONTAINER)
-        compliance_rule={}
+        compliance_rule = {}
         compliance_rule['compliance_name'] = compliance_container.find_element_by_css_selector('header h3').text
         compliance_rule['compliance_status'] = compliance_container.find_element_by_css_selector('div h3').text
         compliance_rule['compliance_date'] = compliance_container.find_element_by_css_selector('div h5 span').text
-        compliance_rule['compliance_policy'] = compliance_container.find_element_by_css_selector('div h5 span').text
+        compliance_rule['compliance_policy'] = compliance_container.find_element_by_css_selector(
+            'section:nth-child(3) label').text
+        compliance_rule['compliance_requirement'] = compliance_container.find_element_by_css_selector(
+            'section:nth-child(4) label').text
+        compliance_rule['compliance_control'] = compliance_container.find_element_by_css_selector(
+            'section:nth-child(5) label').text
+        return compliance_rule
