6ce6eaefa8d364cd4dcebe3b2188bbf129c47878
Calin Marchis <calin.marchis@hpe.com>
2016-11-02 12:53:48 +0200

    update ViewRuleDetails

--- test/ViewRuleDetailsTest.py
+++ test/ViewRuleDetailsTest.py
@@ -3,10 +3,14 @@ import unittest
 from pages.MenuNavigationPage import MenuNavigationPage
 from pages.compliance.CompliancePage import CompliancePage
 from pages.compliance.ComplianceRuleDetailsPage import ComplianceRuleDetailsPage
+from pages.templates.TemplatesMenuListPage import TemplatesMenuListPage
+from pages.templates.details.TemplateDetailsPage import TemplateDetailsPage
+from pages.deployments.DeploymentsPage import DeploymentsPage
 from tools.ConfigUtils import ConfigUtils
 from tools.DriverUtils import DriverUtils
 from tools.ListUtils import ListUtils
 from tools.api.mock.MockApiUtils import MockApiUtils
+from tools.SoftAssert import SoftAssert
 
 
 class ViewRuleDetailsTest(unittest.TestCase):
@@ -43,7 +47,7 @@ class ViewRuleDetailsTest(unittest.TestCase):
         self.random_compliance_id = random_compliance.get('compliance_id')
         print 'self.random_compliance: ', self.random_compliance_id
         self.compliance_details = ListUtils().grab_compliance_details_list(compliance_json, self.random_compliance_id)
-        print 'self.compliance_details: ',self.compliance_details
+        print 'self.compliance_details: ', self.compliance_details
 
         self.browser = DriverUtils().start_driver()
 
@@ -51,22 +55,24 @@ class ViewRuleDetailsTest(unittest.TestCase):
         menu_navigation_page = MenuNavigationPage(self.browser)
         menu_navigation_page.navigate_to("http://localhost:8014/provision")
         menu_navigation_page.navigate_to("http://localhost:8014/provision")
-        # templates_menu_list_page = TemplatesMenuListPage(self.browser)
-        # templates_menu_list_page.click_on_template_item('Oracle Provision Template - Copy(1)')
 
-        # template_details_page = TemplateDetailsPage(self.browser)
-        # template_details_page.select_deployment('Cis - Copy(1)')
-        #
-        # deployment_page = DeploymentsPage(self.browser)
-        # deployment_page.select_resource_by_id('6170')
-
-        menu_navigation_page.click_on_menu_item('Compliance')
+        templates_menu_list_page = TemplatesMenuListPage(self.browser)
+        templates_menu_list_page.click_on_template_item('self.random_templateName')
+        template_details_page = TemplateDetailsPage(self.browser)
+        template_details_page.select_deployment(self.random_deploymentName)
+        deployment_page = DeploymentsPage(self.browser)
+        deployment_page.select_resource_by_id(self.random_resource_id)
 
         compliance_page = CompliancePage(self.browser)
-        compliance_page.click_compliance('9348')
+        compliance_page.click_compliance(self.random_compliance_id)
 
         compliance_rule_details_page = ComplianceRuleDetailsPage(self.browser)
-        print compliance_rule_details_page.create_compliance_rule_details_list_of_dictionary()
+        aplication_compliance_rule_details = compliance_rule_details_page.create_compliance_rule_details_list_of_dictionary()
+
+        SoftAssert().verfy_equals_true("Scan message isn't being displayed properly",
+                                       self.compliance_details, aplication_compliance_rule_details)
+
+        self.assertEqual(SoftAssert().failures_size(), 0, str(SoftAssert().failures_list()))
 
     def tearDown(self):
         menuNavigationPage = MenuNavigationPage(self.browser)
